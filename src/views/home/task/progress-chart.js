8a5969a9dd45bc57cc4502f802ee310511d84187
a2323178080 <a2323178080@gmail.com>
2023-08-07 09:54:43 +0800

    0807 新增progress-chart邊框

--- src/views/home/task/progress-chart.js
+++ src/views/home/task/progress-chart.js
@@ -8,13 +8,15 @@ export default function ProgressChart(){
     const {progressBar} = useColor ()
     const [state] = useContext(CartContext);
     return(
-        <div className="progressChart">
-            <div className={`element1 element1--${state.startCondition}`}
-                 style={{borderColor:`var(--${progressBar})`}}>
-                <div className={`element2 element2--${state.startCondition}`}
-                     style={{borderColor:`var(--${progressBar})`,backgroundColor:`var(--${progressBar})`}}></div>
-                <div className={`element3 element3--${state.startCondition}`}
-                     style={{borderColor:`var(--${progressBar})`,backgroundColor:`var(--${progressBar})`}}></div>
+        <div className="progressChart" >
+            <div className="borderCircle" style={{border:`1px solid var(--${progressBar})`}}>
+                <div className={`element1 element1--${state.startCondition}`}
+                     style={{borderColor:`var(--${progressBar})`}}>
+                    <div className={`element2 element2--${state.startCondition}`}
+                         style={{borderColor:`var(--${progressBar})`,backgroundColor:`var(--${progressBar})`}}></div>
+                    <div className={`element3 element3--${state.startCondition}`}
+                         style={{borderColor:`var(--${progressBar})`,backgroundColor:`var(--${progressBar})`}}></div>
+                </div>
             </div>
         </div>
     )
