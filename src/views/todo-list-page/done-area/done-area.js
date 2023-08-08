50d335fffce613ac00945098f3b6c79f35610cf7
a2323178080 <a2323178080@gmail.com>
2023-08-08 08:41:32 +0800

    0808 done-area commit

---
+++ src/views/todo-list-page/done-area/done-area.js
@@ -0,0 +1,32 @@
+import "./done-area.scss"
+import {useContext} from "react";
+import {CartContext} from "../../../store/store";
+
+export default function DoneArea({position}){
+
+    const [state] = useContext(CartContext);
+
+    const newPosition={
+        lowerMiddle:{
+            left:'475px',
+            top:'498px'
+        },
+
+    }
+
+    const newStyle={
+        ...newPosition[position],
+
+    }
+    return(
+        <div className="doneArea" style={newStyle}>
+            <div className="doneBar">DONE
+                <div className="dropdown"></div>
+            </div>
+
+            {state.doneTodo.map((item)=>{
+                return <div >{item.do}</div>
+            })}
+        </div>
+    )
+}
