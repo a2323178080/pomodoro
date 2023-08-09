be2e70900ddf2d0ef46ad26f7e81e0fd0af58aa4
a2323178080 <a2323178080@gmail.com>
2023-08-09 18:03:59 +0800

    0809 新增測試list

--- src/views/todo-list-page/event-area/list.js
+++ src/views/todo-list-page/event-area/list.js
@@ -1,8 +1,53 @@
 import "./list.scss"
-export default function List(){
+import {useContext} from "react";
+import {CartContext} from "../../../store/store";
+import Circle from "../../../components/circle";
+
+export default function List({position}){
+    const [state] = useContext(CartContext);
+
+    const newPosition = {
+        lowerMiddle: {
+            left: '475px',
+            top: '498px'
+        },
+    }
+    const newStyle = {
+        ...newPosition[position],
+    }
+
+    function handleDot(num) {
+        let list = [];
+        for (let i = 0; i < num; i++) {
+            list.push(<div><Circle size={12} bg="white" border="2px solid #003164"
+                                   marginLeft="eight"/>
+            </div>);
+        }
+        return list;
+    }
+
     return(
-        <div className="list">
-                <div>list測試</div>
+        <div className="list" style={newStyle}>
+            <div className="doneBar">DONE
+                <div className="dropdown"></div>
+            </div>
+
+            <div className="item">
+                {state.doneTodo.map((item) => {
+                    return (
+
+                        <ul>
+                            <li className="item--li">
+                                <span className="material-icons">check_circle_outline</span>
+                                <span>{item.do}</span>
+                                <span className="item--cycleIcon">{handleDot(item.number)}</span>
+                            </li>
+                        </ul>
+
+
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/views/todo-list-page/event-area/title.js
+++ src/views/todo-list-page/event-area/title.js
@@ -1,4 +1,4 @@
-import "./title.scss"
+import "./title.scss";
 export default function Title(){
     return(
         <div className="title">
