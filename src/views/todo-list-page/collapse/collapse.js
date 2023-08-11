ecfe952219c58848bbf5f3858740e0676b5e8647
a2323178080 <a2323178080@gmail.com>
2023-08-11 17:00:59 +0800

    0811 參考ant功能測試使用

---
+++ src/views/todo-list-page/collapse/collapse.js
@@ -0,0 +1,29 @@
+import "./collapse.scss"
+import {useState} from "react";
+export default function Collapse({list}){
+    const [openCondition,setOpenCondition]=useState(true);
+    const handleOpen=()=>{
+        setOpenCondition(pre=>!pre);
+
+    }
+
+
+    return(
+        <div className="collapse">
+            {list.map((event)=>{
+                return (
+
+                    <div>
+                        <div onClick={handleOpen}>
+                            {event.label}
+
+                            {openCondition&&<div>{event.children}</div>}
+                        </div>
+
+
+
+                    </div>)
+            })}
+        </div>
+    )
+}
--- src/views/todo-list-page/todo-list-page.js
+++ src/views/todo-list-page/todo-list-page.js
@@ -1,38 +1,80 @@
 import "./todo-list-page.scss";
-import Input from "../home/input/input";
+import React, {useContext} from "react";
+import {CartContext} from "../../store/store";
+import Logo from "./logo/logo";
 // import TodoArea from "./todo-area/todo-area";
 // import DoneArea from "./done-area/done-area";
-import Logo from "./logo/logo";
-import Title from "./event-area";
+import Input from "../home/input/input";
+// import Title from "./event-area";
 import Slogan from "../../components/slogan"
 import CrossIcon from "../../components/cross-icon";
 import Sidebar from "../../components/sidebar";
 
+import Collapse from "./collapse/collapse";
 
 
 export default function TodoListPage(){
+    const [state] = useContext(CartContext);
+
+
+    const text = "測試用文字";
+
+    const done = [
+        {
+            key: '1',
+            label: 'DONE',
+            children:
+
+                state.doneTodo.map((item)=>{
+                return <ul>
+                    <li className="item--li">
+                        <span className="material-icons">check_circle_outline</span>
+                        <span className="font">{item.do}</span>
+                        <span className="item--cycleIcon"></span>
+                    </li>
+                </ul>
+            })
+
+
+
+
+
+
+
+        }
+    ];
+
+
+
+
+
+
+
     return(
         <div className="todoListPage">
             <div className="layout">
-
                 <div className="col-5">
-
                     <Sidebar position="onTodoListPage"/>
                     <Logo/>
                 </div>
 
-
-                {/*<TodoArea position="center"/>*/}
-                {/*<DoneArea position="lowerMiddle"/>*/}
                 <div className="col-5">
                     <Input />
-                    <Title area="todo" >
-                        <Title.List area="todo"/>
-                    </Title>
 
-                    <Title area="done">
-                        <Title.List area="done"/>
-                    </Title>
+                    {/*<TodoArea position="center"/>*/}
+                    {/*<DoneArea position="lowerMiddle"/>*/}
+    {/*---------------------------------------------------------*/}
+                    {/*<Title area="todo" >*/}
+                    {/*    <Title.List area="todo"/>*/}
+                    {/*</Title>*/}
+
+                    {/*<Title area="done">*/}
+                    {/*    <Title.List area="done"/>*/}
+                    {/*</Title>*/}
+    {/*---------------------------------------------------------*/}
+
+
+                    <Collapse list={done}/>
                 </div>
 
                 <div>
