2bd833eacbbc7c88eb0aeee470e3e142921ada90
a2323178080 <a2323178080@gmail.com>
2023-08-14 14:18:22 +0800

    0814 調整input  plus icon位置

--- src/views/home/input/input.js
+++ src/views/home/input/input.js
@@ -1,5 +1,5 @@
 import {CartContext} from "../../../store/store";
-import {useState, useContext, useEffect} from "react";
+import {useState, useContext } from "react";
 import "./input.scss";
 import {PlusOutlined} from "@ant-design/icons";
 import useColor from "../../../hook/useColor";
