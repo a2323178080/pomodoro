adb70cc7b19100c633babc876ab7a68dcf155679
a2323178080 <a2323178080@gmail.com>
2023-08-15 10:59:53 +0800

    0815 存版本

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,21 +3,11 @@ import {Link} from "react-router-dom";
 
 export default function Sidebar({position}){
 
-    const newStyle={
-        onTodoListPage:{
-            color:'var(--pink)'
-        },
-        onAnalyticsPage:{
-            color:'var(--pink)'
-        }
-    }
-
-
     return(
         <div className="sidebar">
 
-            <Link  to="/todoListPage" className={`fa fa-list-ul fa-2x homeBackground--iconContent--listIcon ${position==='onAnalyticsPage'?'pink':'gray'}`}>
-                <span className={`font ${position==='onAnalyticsPage'?'pink':'gray'}`} >TO-DO LIST</span>
+            <Link  to="/todoListPage" className={`fa fa-list-ul fa-2x homeBackground--iconContent--listIcon ${position==='onTodoListPage'?'pink':'gray'}`}>
+                <span className={`font ${position==='onTodoListPage'?'pink':'gray'}`} >TO-DO LIST</span>
             </Link>
 
             <Link to="/analytics" className={`fas fa-chart-bar fa-2x homeBackground--iconContent--chartIcon ${position==='onAnalyticsPage'?'pink':'gray'}`} >
