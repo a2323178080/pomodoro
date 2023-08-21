394b853cb7184c7fdd987b71d26eff166473a87d
a2323178080 <a2323178080@gmail.com>
2023-08-21 10:57:58 +0800

    0821 Chart新增css

--- src/views/analytics/chart/chart.js
+++ src/views/analytics/chart/chart.js
@@ -2,7 +2,33 @@ import "./chart.scss"
 export default function Chart(){
     return(
         <div className="chart">
-            chart 測
+            <div className="d-flex">
+                <div className="count">
+                    <p>24</p>
+                    <p>20</p>
+                    <p>16</p>
+                    <p>12</p>
+                    <p>8</p>
+                    <p>4</p>
+
+                </div>
+                <div className="content">
+
+                </div>
+            </div>
+
+            <div className="day">
+                <p>7/1</p>
+                <p>7/2</p>
+                <p>7/3</p>
+                <p>7/4</p>
+                <p>7/5</p>
+                <p>7/6</p>
+                <p>7/7</p>
+
+            </div>
+
+
         </div>
     )
 }
